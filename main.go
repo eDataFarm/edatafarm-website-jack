b360279be1285eac524f158f7cb3f25b4847be52
Davis Wamola <dawa009@yahoo.com>
2018-09-06 22:02:34 -0700

    Add auth0

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-  "net/http"
-  "strconv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 
-  "github.com/gin-gonic/contrib/static"
-  "github.com/gin-gonic/gin"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 )
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
 
 // Let's create our Jokes struct. This will contain information about a Joke
 
@@ -28,30 +52,101 @@ var jokes = []Joke{
   Joke{7, 0, "How does a penguin build it's house? Igloos it together."},
 }
 
+var jwtMiddleWare *jwtmiddleware.JWTMiddleware
+
 func main() {
-  // Set the router as the default one shipped with Gin
-  router := gin.Default()
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			aud := os.Getenv("AUTH0_API_AUDIENCE")
+			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAudience {
+				return token, errors.New("Invalid audience.")
+			}
+			// verify iss claim
+			iss := os.Getenv("AUTH0_DOMAIN")
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("Invalid issuer.")
+			}
 
-  // Serve frontend static files
-  router.Use(static.Serve("/", static.LocalFile("./views/js", true)))
+			cert, err := getPemCert(token)
+			if err != nil {
+				log.Fatalf("could not get cert: %+v", err)
+			}
 
-  // Setup route group for the API
-  api := router.Group("/api")
-  {
-    api.GET("/", func(c *gin.Context) {
-      c.JSON(http.StatusOK, gin.H {
-        "message": "pong",
-      })
-    })
-  }
-  // Our API will consit of just two routes
-  // /jokes - which will retrieve a list of jokes a user can see
-  // /jokes/like/:jokeID - which will capture likes sent to a particular joke
-  api.GET("/jokes", JokeHandler)
-  api.POST("/jokes/like/:jokeID", LikeJoke)
+			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+			return result, nil
+		},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
 
-  // Start and run the server
-  router.Run(":3000")
+	jwtMiddleWare = jwtMiddleware
+
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./views/js", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+		api.GET("/jokes", authMiddleware(), JokeHandler)
+		api.POST("/jokes/like/:jokeID", authMiddleware(), LikeJoke)
+	}
+
+	// Start and run the server
+	router.Run(":3000")
+}
+
+func getPemCert(token *jwt.Token) (string, error) {
+	cert := ""
+	resp, err := http.Get(os.Getenv("AUTH0_DOMAIN") + ".well-known/jwks.json")
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	x5c := jwks.Keys[0].X5c
+	for k, v := range x5c {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + v + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		return cert, errors.New("unable to find appropriate key")
+	}
+
+	return cert, nil
+}
+
+// authMiddleware intercepts the requests, and check for a valid jwt token
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get the client secret key
+		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
+		if err != nil {
+			// Token not found
+			fmt.Println(err)
+			c.Abort()
+			c.Writer.WriteHeader(http.StatusUnauthorized)
+			c.Writer.Write([]byte("Unauthorized"))
+			return
+		}
+	}
 }
 
 // JokeHandler retrieves a list of available jokes
